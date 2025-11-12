eb7550b26de1bc961da974b0f40202ad0617ebbb
TechTastic <thetechnomancer123@gmail.com>
2025-11-11 20:19:42 -0600

    So this crashes servers...

--- bounty-hunting/bountyboard/interact.js
+++ bounty-hunting/bountyboard/interact.js
@@ -1,21 +1,30 @@
 function hasBounty() {
-    return event.player.getStoredData("existing_bounty") == true;
+    return event.player.getStoredData("existing_bounty") === true;
 }
 
-function isDimensionBlacklisted(world) {
-    return false;
+function getValidWorlds() {
+    return [API.getIWorldLoad(79), API.getIWorldLoad(4), API.getIWorldLoad(5), API.getIWorldLoad(3), API.getIWorldLoad(80)];
+}
+
+function isDimensionWhitelisted(world) {
+    var id = world.getDimensionID();
+    return id == 79 || id == 4 || id == 5 || id == 3 || id == 80;
 }
 
 function getRandomDimension() {
-    var worlds = array.filter(world => {return !isDimensionBlacklisted(world)}, API.getWorlds());
+    var worlds = getValidWorlds();
     return worlds[Math.floor(Math.random() * worlds.length)];
 }
 
-function getRandomPosition(x, z, radius) {
+function getRandomPosition(world, x, z, radius) {
     var randomX = x + (Math.random() * radius * 2) - radius;
     var randomZ = z + (Math.random() * radius * 2) - radius;
-    var highestY = world.getBlock(randomX, 255, randomZ).getHeight();
-    return randomX, highestY, randomZ;
+    for (var y = 255; y > 0; y++) {
+        if (world.getBlock(randomX, y, randomZ) != null) {
+            return randomX, y + 1, randomZ;
+        }
+    }
+    return randomX, 70, randomZ;
 }
 
 function getQuestCategory(name) {
@@ -28,27 +37,20 @@ function getQuestCategory(name) {
     return null;
 }
 
-// Test for existing bounty
-// If bounty, refute
 if (hasBounty()){
     event.setCancelled(true);
-    return;
 }
 
-// Spawn new NPC in a random dimension and location
 var world = getRandomDimension();
-var x, y, z = getRandomPosition(0, 0, 500);
-var target = API.spawnNpc(world, x, y, z);
+var x, y, z = getRandomPosition(world, 0, 0, 500);
+var target = API.spawnNPC(world, x, y, z);
 
-// Create new Quest with necessary info
 var category = getQuestCategory("Bounty Hunting");
 var quest = category.create();
-quest.setCompleteText("Good job, we will be in touch again.")
-quest.setLogText("Your target is " + target.getName() + ".\nThey are on " + world.getDimensionID() + " around " + x + ", " + y + ", " + z + ".\n\nDo not fail us.")
-quest.setName("The " + target.getName() + " Contract")
-quest.setType(2); // kill
-// set target - QuestKill?
-// set rewards
-
-// Assign quest to player
-// Give Puck?
+quest.setCompleteText("Good job, we will be in touch again.");
+quest.setLogText("Your target is " + target.getName() + ".\nThey are on " + world.getDimensionID() + " around " + x + ", " + y + ", " + z + ".\n\nDo not fail us.");
+quest.setName("The " + target.getName() + " Contract");
+quest.setType(5);
+quest.command = "pay @dp 1000";
+quest.save();
+// Still need to assign to player
